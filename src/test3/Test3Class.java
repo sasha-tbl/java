6fef95e9f3498b63d233620905ea1374a6e8e5cc
sasha-tbl <sasha_tbl@yahoo.com>
2023-05-10 12:28:42 +0300

    Test commit 10/05, 12:28

---
+++ src/test3/Test3Class.java
@@ -0,0 +1,8 @@
+package test3;
+
+public class Test3Class {
+	public void Test3Class()
+	{
+		System.out.println("Object created");
+	}
+}
---
+++ src/test3/Test3ClassTest.java
@@ -0,0 +1,19 @@
+package test3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Test3ClassTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTest3Class() {
+		
+	}
+
+}
